ce3c290dbbb3dc492f55a7750e9b4077b991c060
Josh Menzel <josh.menzel@yahoo.com>
2016-12-27 19:09:58 -0600

    Working with 14 teams or more...

--- VEXDisplay/Classes/panels/RanksPanel.py
+++ VEXDisplay/Classes/panels/RanksPanel.py
@@ -11,7 +11,7 @@ from ..EventData import *
 
 global RANK_PANEL_HEIGHT
 global RANK_PANEL_DELTA_Y
-RANK_PANEL_DELTA_Y = 2
+RANK_PANEL_DELTA_Y = 1
 RANK_PANEL_HEIGHT = 76
 class RanksPanel(wx.Panel):
     global EVENT_DATA
@@ -23,21 +23,37 @@ class RanksPanel(wx.Panel):
         self.SetBackgroundColour(COLORS["White"])
         self.rankPanels = OrderedDict()
         self.extraPanels = OrderedDict()
-        self.positionY = 0
         self.SetDoubleBuffered(True)
         self.logoPanel = None
         
         self.curSponsorI = -1
         self.updatePositionsTimer = None
         self.changeLogoTimer = None
+        self.lastRank = -1
+        
+        self.lastRankLoc = 0
+        self.unshownRanks = OrderedDict()
+        self.logoPatternOff = 0
         self.setupRankPanels(speed=speed)
 
-        self.cancel = False
+        self.lastY = 0
     def setupRankPanels(self,redraw=False,speed=270):
+        self.lastRank = -1
         self.yOff = 0
         self.isGray = False
-        for rank in EVENT_DATA.ranks:
-            if rank not in self.rankPanels:
+        
+        if len(EVENT_DATA.sponsors) > 0:
+            self.curSponsorI = 0
+            self.logoPanel = RankLogoPanel_Main(self,EVENT_DATA.sponsors[self.curSponsorI])
+            self.logoPanel.Move((0,self.yOff),wx.SIZE_ALLOW_MINUS_ONE)
+            self.logoPanel.SetBackgroundColour(COLORS["White"])
+            self.logoPanel.Show()
+            self.yOff += self.logoPanel.GetSize()[1]
+        self.lastRankLoc = self.yOff
+        for i in range(len(EVENT_DATA.ranks)):
+            rank = str(i+1)
+            
+            if i < 14:
                 self.rankPanels[rank] = TeamRankPanel_Main(self,rank)
                 self.rankPanels[rank].Move((0,self.yOff),wx.SIZE_ALLOW_MINUS_ONE)
                 if self.isGray:
@@ -46,88 +62,80 @@ class RanksPanel(wx.Panel):
                     self.rankPanels[rank].SetBackgroundColour(COLORS["vexBGDarkGray"])
                 self.isGray = not self.isGray
                 self.yOff += RANK_PANEL_HEIGHT
-        if len(self.rankPanels) > 0:
-            while ((RANK_PANEL_HEIGHT*len(self.rankPanels)) + (RANK_PANEL_HEIGHT*len(self.extraPanels))) < (900+RANK_PANEL_HEIGHT):
-                key = str(len(self.extraPanels)+1)
+                self.lastRankLoc += RANK_PANEL_HEIGHT
+                self.lastRank = i
+                
+            else:
+                self.lastRankLoc += RANK_PANEL_HEIGHT
         
+
+       
+        
+
+
+        if len(self.rankPanels) > 0:
+            while len(self.rankPanels) + len(self.extraPanels) < 15:
+                key = str(len(self.extraPanels)+1)
                 self.extraPanels[key] = TeamRankPanel_Main(self)
                 self.extraPanels[key].Move((0,self.yOff),wx.SIZE_ALLOW_MINUS_ONE)
                 self.yOff += RANK_PANEL_HEIGHT
                 self.extraPanels[key].SetBackgroundColour(COLORS["White"])
-        if len(EVENT_DATA.sponsors) > 0:
-            self.curSponsorI = 0
-            self.logoPanel = RankLogoPanel_Main(self,EVENT_DATA.sponsors[self.curSponsorI])
-            self.logoPanel.Move((0,self.yOff),wx.SIZE_ALLOW_MINUS_ONE)
-            self.logoPanel.SetBackgroundColour(COLORS["White"])
-            self.logoPanel.Show()
 
-        self.positionY = self.yOff-RANK_PANEL_HEIGHT
+
+
+
         self.updatePositionsTimer = wx.Timer(self,-1)
         self.Bind(wx.EVT_TIMER,self.updatePositions,self.updatePositionsTimer)
         self.updatePositionsTimer.Start(1000.0/speed)
         self.changeLogoTimer =wx.Timer(self,-1)
         self.Bind(wx.EVT_TIMER,self.updateRankSponsorLogo,self.changeLogoTimer)
         self.changeLogoTimer.Start(10000)
-        #self.updatePositionsTimer.Start(500)
-        #self.logoPanel = RankLogoPanel_Main(self,EVENT_DATA.sponsors[0])
-        #wx.CallAfter(self.updatePositions)
+
                 
     def updatePositions(self,evt=None):
         cancel = False
-        try:
-            for i in range(len(EVENT_DATA.ranks)):
-                
-                if str(i+1) in self.rankPanels:
-                    panelRef = self.rankPanels.items()[i][1]
+        for panel in self.rankPanels:
+            panelRef = self.rankPanels[panel]
 
-                    if self.rankPanels[str(i+1)].GetPosition()[1] < (-1*self.rankPanels[str(i+1)].GetSize()[1]):
-                        newY = i*panelRef.GetSize()[1] + self.rankPanels[str(len(self.rankPanels))].GetPosition()[1] + self.rankPanels[str(len(self.rankPanels))].GetSize()[1]
-                        if self.logoPanel != None:
-                            newY += self.logoPanel.GetSize()[1]
-                        newY += len(self.extraPanels)*RANK_PANEL_HEIGHT
-                        panelRef.Move((0,newY-RANK_PANEL_DELTA_Y),wx.SIZE_ALLOW_MINUS_ONE)
-                        #if panelRef.rank in EVENT_DATA.ranks:
-                        #    panelRef.updateLabelTexts()
-                        #else:
-                        if not panelRef.rank in EVENT_DATA.ranks:
-                            if (i+1) > 11:
-                                del self.rankPanels[panelRef.rank]
-                                panelRef.Destroy()
-                            else:
-                                panelRef.rank = None
-                                panelRef.updateLabelTexts()
-                    else:
-                        panelRef.Move((0,panelRef.GetPosition()[1]-RANK_PANEL_DELTA_Y),wx.SIZE_ALLOW_MINUS_ONE)
+            if panelRef.GetPosition()[1] < (-1*panelRef.GetSize()[1]):
+                newY = (int(panel)-1)*panelRef.GetSize()[1] + self.rankPanels[str(len(self.rankPanels))].GetPosition()[1] + self.rankPanels[str(len(self.rankPanels))].GetSize()[1]
+                #if self.logoPanel != None:
+                #    newY += self.logoPanel.GetSize()[1]
+               
+                if self.lastRank < len(EVENT_DATA.ranks)-1:
+                        self.lastRank += 1
                 else:
-                    cancel = True
-                    break
+                    self.lastRank = 0
 
-            if cancel == False:
-                for i in range(len(self.extraPanels)):
-                    panelRef = self.extraPanels[str(i+1)]
-                    if panelRef.GetPosition()[1] < (-1*panelRef.GetSize()[1]):
-                        newY = i*panelRef.GetSize()[1] + self.rankPanels[str(len(self.rankPanels))].GetPosition()[1] + self.rankPanels[str(len(self.rankPanels))].GetSize()[1]
-                        #if self.logoPanel != None:
-                        #    newY += self.logoPanel.GetSize()[1]
-                        #newY += len(self.extraPanels)*90
-                        panelRef.Move((0,newY),wx.SIZE_ALLOW_MINUS_ONE)
-                    else:
-                        panelRef.Move((0,panelRef.GetPosition()[1]-RANK_PANEL_DELTA_Y),wx.SIZE_ALLOW_MINUS_ONE)
-                if self.logoPanel != None:
-                    if self.logoPanel.GetPosition()[1] < (-1*self.logoPanel.GetSize()[1]):
-                        newY = self.rankPanels[str(len(self.rankPanels))].GetPosition()[1] + self.rankPanels[str(len(self.rankPanels))].GetSize()[1] 
-                        newY += len(self.extraPanels)*RANK_PANEL_HEIGHT
-                        self.logoPanel.Move((0,newY),wx.SIZE_ALLOW_MINUS_ONE)
-                    else:
-                        self.logoPanel.Move((0,self.logoPanel.GetPosition()[1]-RANK_PANEL_DELTA_Y),wx.SIZE_ALLOW_MINUS_ONE)    
-                #wx.CallLater(18,self.updatePositions)
+                if self.lastRank == 0:
+                    self.logoPanel.Move((0,newY+(len(self.extraPanels)*RANK_PANEL_HEIGHT)+self.logoPatternOff),wx.SIZE_ALLOW_MINUS_ONE)
+                    self.logoPatternOff = self.logoPanel.GetSize()[1]
+                elif panel == "1":
+                    self.logoPatternOff = 0
+                newY += len(self.extraPanels)*RANK_PANEL_HEIGHT+self.logoPatternOff
+                panelRef.Move((0,newY-RANK_PANEL_DELTA_Y),wx.SIZE_ALLOW_MINUS_ONE)
+
+                panelRef.updateLabelTexts(str(self.lastRank+1))#update panel to show the new rank
+
+                print str(panel) + " | "  + str(self.logoPatternOff) + " | " + str(panelRef.GetPosition()[1]+RANK_PANEL_DELTA_Y)
             else:
-                self.regenScroll()
-            if evt != None:
-                evt.Skip()
-        
-        except Exception, ex:
-            doNothing = True
+                panelRef.Move((0,panelRef.GetPosition()[1]-RANK_PANEL_DELTA_Y),wx.SIZE_ALLOW_MINUS_ONE)
+
+
+
+        for i in range(len(self.extraPanels)):
+            panelRef = self.extraPanels[str(i+1)]
+            if panelRef.GetPosition()[1] < (-1*panelRef.GetSize()[1]):
+                #newY = i*panelRef.GetSize()[1] + self.rankPanels[str(len(self.rankPanels))].GetPosition()[1] + self.rankPanels[str(len(self.rankPanels))].GetSize()[1]
+                newY = (i+1)*RANK_PANEL_HEIGHT + self.rankPanels[str(len(EVENT_DATA.ranks))].GetPosition()[1]
+
+                panelRef.Move((0,newY),wx.SIZE_ALLOW_MINUS_ONE)
+            else:
+                panelRef.Move((0,panelRef.GetPosition()[1]-RANK_PANEL_DELTA_Y),wx.SIZE_ALLOW_MINUS_ONE)
+
+        if self.logoPanel != None:
+            self.logoPanel.Move((0,self.logoPanel.GetPosition()[1]-RANK_PANEL_DELTA_Y),wx.SIZE_ALLOW_MINUS_ONE) 
+            
     def regenScroll(self):
         self.updatePositionsTimer.Stop()
         self.changeLogoTimer.Stop()
@@ -153,9 +161,4 @@ class RanksPanel(wx.Panel):
                 
             self.logoPanel.updateImg(EVENT_DATA.sponsors[self.curSponsorI])
             self.logoPanel.SetBackgroundColour(COLORS["White"])
-        ###CHECK ALL PANELS###
-        if len(self.rankPanels) != len(EVENT_DATA.ranks):
-            self.regenScroll()
-
-        #######################
         evt.Skip()
--- VEXDisplay/Classes/panels/TeamRankPanel_Main.py
+++ VEXDisplay/Classes/panels/TeamRankPanel_Main.py
@@ -12,28 +12,36 @@ class TeamRankPanel_Main(wx.Panel):
         
         self.SetSize((1300,76))
         self.rank = rank
-        if self.rank != None:
-            self.lblRankNumber = wx.StaticText(self,-1)
-            self.lblRankNumber.SetForegroundColour(COLORS["vexTxtGray"])
-            self.lblRankNumber.SetFont(FONTS["NotoSansBold_56"])
-            self.lblRankNumber.SetLabel(str(rank))
-            self.lblRankNumber.SetPosition((((120-self.lblRankNumber.GetSize()[0])/2),(self.GetSize()[1]-self.lblRankNumber.GetSize()[1])/2))
+        #if self.rank != None:
+        self.lblRankNumber = wx.StaticText(self,-1)
+        self.lblRankNumber.SetForegroundColour(COLORS["vexTxtGray"])
+        self.lblRankNumber.SetFont(FONTS["NotoSansBold_56"])
+    #     self.lblRankNumber.SetLabel(str(rank))
+    #     self.lblRankNumber.SetPosition((((120-self.lblRankNumber.GetSize()[0])/2),(self.GetSize()[1]-self.lblRankNumber.GetSize()[1])/2))
         
-            self.lblTeamNumber = wx.StaticText(self,-1)
-            self.lblTeamNumber.SetForegroundColour(COLORS["vexTxtGray"])
-            #self.lblTeamNumber.SetFont(FONTS[""])
-            self.lblTeamName = wx.StaticText(self,-1)
-            self.lblAPs = wx.StaticText(self,-1)
-            self.lblSPs = wx.StaticText(self,-1)
-            self.lblWPs = wx.StaticText(self,-1)
-            self.lblWins = wx.StaticText(self,-1)
-            self.lblWinsD = wx.StaticText(self,-1)
-            self.lblLosses = wx.StaticText(self,-1)
-            self.lblLossesD = wx.StaticText(self,-1)
-            self.lblTies = wx.StaticText(self,-1)
-    def updateLabelTexts(self):
+        self.lblTeamNumber = wx.StaticText(self,-1)
+        self.lblTeamNumber.SetForegroundColour(COLORS["vexTxtGray"])
+        #self.lblTeamNumber.SetFont(FONTS[""])
+        self.lblTeamName = wx.StaticText(self,-1)
+        self.lblAPs = wx.StaticText(self,-1)
+        self.lblSPs = wx.StaticText(self,-1)
+        self.lblWPs = wx.StaticText(self,-1)
+        self.lblWins = wx.StaticText(self,-1)
+        self.lblWinsD = wx.StaticText(self,-1)
+        self.lblLosses = wx.StaticText(self,-1)
+        self.lblLossesD = wx.StaticText(self,-1)
+        self.lblTies = wx.StaticText(self,-1)
+        self.updateLabelTexts(self.rank)
+    def updateLabelTexts(self,rank = None):
+        self.rank = rank
         if self.rank != None:
-            d = True
+            if self.rank != "":
+                self.lblRankNumber.SetLabel(str(rank))
+                self.lblRankNumber.SetPosition((((120-self.lblRankNumber.GetSize()[0])/2),(self.GetSize()[1]-self.lblRankNumber.GetSize()[1])/2))
+            else:
+                self.updateLabelTexts()
         else:
-            d = True
+            self.lblRankNumber.SetLabel("")
+            self.lblRankNumber.Hide()
+        self.Refresh()
     
